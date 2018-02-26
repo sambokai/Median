d417354b1011f36064c8947c185bf5c1a2c45ba9
Sam Bokai <sam.bokai@scout24.com>
2018-02-26 21:55:56 +0100

    Refactor; Stub routes and db query methods; Prepare part of project structure

--- app/database/ArticleTable.scala
+++ app/database/ArticleTable.scala
@@ -1,30 +1,26 @@
 package database
 
-import java.sql.Date
+import java.sql.Timestamp
 
-import play.api.libs.json._
+import domain.Article
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.{Query, TableQuery, Tag}
 
-case class Article(id: Int, user: Int, body: String, created: Date)
-
-object Article {
-  implicit val articleFormat: OFormat[Article] = Json.format[Article]
-}
-
-class ArticleTable(tag: Tag) extends Table[Article](tag, "ARTICLES") {
-
-  def id = column[Int]("article_id", O.AutoInc, O.PrimaryKey)
-
-  def user = column[Int]("user_id")
-
-  def body = column[String]("body")
-
-  def created = column[Date]("created_on")
-
-  def * = (id, user, body, created) <> ((Article.apply _).tupled, Article.unapply)
-}
-
 object ArticleTable {
+
+  class ArticleTable(tag: Tag) extends Table[Article](tag, "ARTICLES") {
+
+    def id = column[Int]("article_id", O.AutoInc, O.PrimaryKey)
+
+    def user = column[Int]("user_id")
+
+    def body = column[String]("body")
+
+    def created = column[Timestamp]("created_on")
+
+    def * = (id, user, body, created) <> ((Article.apply _).tupled, Article.unapply)
+  }
+
   val articles: Query[ArticleTable, Article, Seq] = TableQuery[ArticleTable]
 }
+
