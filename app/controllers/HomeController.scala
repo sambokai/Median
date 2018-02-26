7e9f9b073dde3dfadf8d2e5fac25a8d108fad76d
Sam Bokai <sam.bokai@scout24.com>
2018-02-26 16:50:25 +0100

    Setup database access with slick; Implement basic getAllUsers query; Display users in view; Remove unused play project template boilerplate;

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,35 @@
 package controllers
 
 import javax.inject._
+
+import data.{Article, User}
 import play.api.mvc._
+import services.UserFinder
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
+
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(userFinder: UserFinder, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+    val users: Seq[User] = Await.result(userFinder.getUsers, Duration.Inf)
+
+    val result: Seq[String] = users.map(user => s"${user.username} was born on ${user.birthday}. His/Her Email is '${user.email}'")
+
+    Ok(views.html.index(result))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -10,8 +10,23 @@ resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
       
 scalaVersion := "2.12.2"
 
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
+libraryDependencies ++= Seq( ehcache , ws , specs2 % Test , guice )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
-      
+// Slick
+libraryDependencies ++= Seq(
+  "com.typesafe.slick" %% "slick" % "3.2.1",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.2.1",
+
+  // MySQL Driver
+  "mysql" % "mysql-connector-java" % "5.1.34"
+
+)
+
+// PlaySlick
+libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play-slick" % "3.0.1",
+  "com.typesafe.play" %% "play-slick-evolutions" % "3.0.1"
+)
