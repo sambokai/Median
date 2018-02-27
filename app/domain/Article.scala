824ab627d82eaebe5879edcb429401cd6779f168
Sam Bokai <sam.bokai@scout24.com>
2018-02-27 22:36:52 +0100

    Add dependencies joda-time and slick-joda-mapper. Use them instead of java datetime and java.sql.Timestamp

--- app/domain/Article.scala
+++ app/domain/Article.scala
@@ -1,9 +1,10 @@
 package domain
 
-import java.sql.Timestamp
+import com.github.nscala_time.time.Imports._
 
 
-case class Article(id: Int, user: Int, body: String, created: Timestamp)
+
+case class Article(id: Int, user: Int, body: String, created: DateTime)
 
 object Article {
   //TODO: custom json conversion https://stackoverflow.com/questions/40406440/convert-json-to-joda-datetime-in-play-scala-2-5
