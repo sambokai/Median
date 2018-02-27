db4e9848f4625c2bd1c1f47db6e1baca8cc93713
Sam Bokai <sam.bokai@scout24.com>
2018-02-27 10:50:32 +0100

    Implement method for retrieving latest articles (feed); Minor reformat;

--- app/controllers/ArticleFeedController.scala
+++ app/controllers/ArticleFeedController.scala
@@ -10,11 +10,11 @@ import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
-class ArticleFeedController @Inject()(userFinder: UserArticleService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class ArticleFeedController @Inject()(userArticleService: UserArticleService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
 
   def index: Action[AnyContent] = Action.async {
-    val users: Future[Seq[User]] = userFinder.getUsers
+    val users: Future[Seq[User]] = userArticleService.getUsers
 
     val outputs: Future[Seq[String]] = users.map(_.map(user => s"${user.username} was born on ${user.birthday}. His/Her Email is '${user.email}'"))
 
--- app/services/UserArticleService.scala
+++ app/services/UserArticleService.scala
@@ -2,6 +2,7 @@ package services
 
 import javax.inject.{Inject, Singleton}
 
+import database.ArticleTable.articles
 import database.UserTable.users
 import domain.{Article, User}
 import play.api.db.slick.DatabaseConfigProvider
@@ -20,7 +21,19 @@ class UserArticleService @Inject()(dbConfigProvider: DatabaseConfigProvider) {
 
   def getUsers: Future[Seq[User]] = db.run(users.result)
 
-  def getUserArticles(from: Int, to: Int): Future[Map[User, Seq[Article]]] = ???
+  def getLatestArticles(limit: Int, offset: Int): Future[Seq[(Article, User)]] = {
+    val userArticles = for {
+      (a, u) <- articles join users on (_.user === _.id)
+    } yield (a, u)
+
+    val query = userArticles
+      .sortBy(_._1.created.desc)
+      .drop(offset)
+      .take(limit)
+      .result
+
+    db.run(query)
+  }
 
   def postArticle(userId: Int, body: String): Future[Int] = ???
 
