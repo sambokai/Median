f8801639b55eec711972d1dbb9aaddfa59c2796d
Sam Bokai <sam.bokai@scout24.com>
2018-02-27 13:36:45 +0100

    Display feed of 5 latest articles on /feed page

--- app/controllers/ArticleFeedController.scala
+++ app/controllers/ArticleFeedController.scala
@@ -2,25 +2,24 @@ package controllers
 
 import javax.inject._
 
-import domain.User
 import play.api.mvc._
 import services.UserArticleService
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 
 @Singleton
 class ArticleFeedController @Inject()(userArticleService: UserArticleService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
+  private val pageSize = 5
 
   def index: Action[AnyContent] = Action.async {
-    val users: Future[Seq[User]] = userArticleService.getUsers
+    val articles = userArticleService.getLatestArticles(pageSize, 0)
 
-    val outputs: Future[Seq[String]] = users.map(_.map(user => s"${user.username} was born on ${user.birthday}. His/Her Email is '${user.email}'"))
+    articles.map { eventualArticles =>
+      Ok(views.html.articleFeed(eventualArticles))
+    }
 
-    outputs.map(eventualOutput =>
-      Ok(views.html.articleFeed(eventualOutput))
-    )
   }
 
 }
