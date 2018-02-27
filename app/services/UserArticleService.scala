ad9f0c5f0f71afa195574959ce9a47080fa3bc2e
Sam Bokai <sam.bokai@scout24.com>
2018-02-27 11:10:51 +0100

    Implement method for posting an article; Document UserArticleService methods using scaladoc comments;

--- app/services/UserArticleService.scala
+++ app/services/UserArticleService.scala
@@ -1,5 +1,7 @@
 package services
 
+import java.sql.Timestamp
+import java.time.Instant
 import javax.inject.{Inject, Singleton}
 
 import database.ArticleTable.articles
@@ -19,8 +21,18 @@ class UserArticleService @Inject()(dbConfigProvider: DatabaseConfigProvider) {
 
   import dbConfig._
 
+
+  /**
+    * @return All registered users
+    */
   def getUsers: Future[Seq[User]] = db.run(users.result)
 
+
+  /**
+    * @param limit  Maximum number of article/user pairs to be retrieved. Commonly used for pagination.
+    * @param offset Index of article to start retrieving until `limit`. Commonly used for pagination.
+    * @return Article/User pair sorted by article creation date and within the range defined by `limit` and `offset`.
+    */
   def getLatestArticles(limit: Int, offset: Int): Future[Seq[(Article, User)]] = {
     val userArticles = for {
       (a, u) <- articles join users on (_.user === _.id)
@@ -35,6 +47,15 @@ class UserArticleService @Inject()(dbConfigProvider: DatabaseConfigProvider) {
     db.run(query)
   }
 
-  def postArticle(userId: Int, body: String): Future[Int] = ???
+
+  /**
+    * @param userId Author of the article to be posted.
+    * @param body   Text to be posted
+    * @return Article id of the posted article.
+    */
+  def postArticle(userId: Int, body: String): Future[Int] = {
+    val query = (articles returning articles.map(_.id)) += Article(0, userId, body, Timestamp.from(Instant.now))
+    db.run(query)
+  }
 
 }
