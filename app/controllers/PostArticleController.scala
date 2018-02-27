bbebb21d81d77c80819991dad25b1cfc364a4700
Sam Bokai <sam.bokai@scout24.com>
2018-02-27 21:45:28 +0100

    Implement article-posting form page

--- app/controllers/PostArticleController.scala
+++ app/controllers/PostArticleController.scala
@@ -2,14 +2,30 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.{AbstractController, ControllerComponents, MessagesActionBuilder}
+import play.api.mvc._
+import services.UserArticleService
+import views.form.PostArticleForm
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class PostArticleController @Inject()(messagesActionBuilder: MessagesActionBuilder, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+class PostArticleController @Inject()(userArticleService: UserArticleService, messagesActionBuilder: MessagesActionBuilder, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
-  def index = TODO
+  def index: Action[AnyContent] = Action.async { implicit request =>
+    userArticleService.getUsers.map { users =>
+      Ok(views.html.postArticle(PostArticleForm.postArticleForm, users, postUrl))
+    }
+  }
 
-  def submitArticle = TODO
+  private val postUrl = routes.PostArticleController.submitArticle()
+
+  def submitArticle = Action { implicit request =>
+    val formData = PostArticleForm.postArticleForm.bindFromRequest.data
+
+    userArticleService.postArticle(formData("userId").toInt, formData("body"))
+
+
+    Redirect("/feed")
+
+  }
 }
